a9d34fa09f31d6dc23a6175a3fcf5ee38f6d8239
Yue Xu <yux154@ucsd.edu>
2017-01-26 10:15:12 -0800

    personalwebsite

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,33 @@
+
+
+
+
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
 
+})
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
-	});
-
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+});
+	$("a.thumbnail").click(projectClick);
+}
+function projectClick(e) {
+	e.preventDefault();
+	//$(this).css("background-color","#7fff00");*/
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
